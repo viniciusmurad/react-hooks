887a765acedce2b54ae638aa7fe5049c340659a3
Vinicius <vm.murad@gmail.com>
2020-01-27 17:35:24 -0300

    hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-    const [tech, setTech] = useState(['ReactJS', 'React Native', 'NodeJS']);
+    const [tech, setTech] = useState([]);
     const [newTech, setNewTech] = useState('');
 
     function handleAdd() {
         setTech([...tech, newTech]);
     }
+
+    useEffect(() => {
+        const storageTech = localStorage.getItem('tech');
+
+        if (storageTech) {
+            setTech(JSON.parse(storageTech));
+        }
+
+        // Like unmount life ciclye
+        // return () => {};
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('tech', JSON.stringify(tech));
+    }, [tech]);
 
     return (
         <>
